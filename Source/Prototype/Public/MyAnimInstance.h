a60d1be2ee8e3b53b8ed1d947daa77f8303d0c89
RZS211 <yyyjjun@gmail.com>
2025-05-19 18:16:54 +0900

    애니메이션 1차 완성
    애니메이션 1차 완성

---
+++ Source/Prototype/Public/MyAnimInstance.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE_API UMyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	UMyAnimInstance();
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	void PlayAttackMontage();
+	void JumpToSection(int32 SectionIndex);
+
+	FName GetAttackMontageName(int32 SectionIndex);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	bool IsFalling;
+
+
+};
