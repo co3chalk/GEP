c0af01b9d56ff898ede347a2df2954a1a8811fe8
Creater-LS <2011dltjs7@naver.com>
2025-05-20 21:28:00 +0900

    흠냐륑 에너지 액터 부모 만드는중
    부모는 거의 프로토타입 이걸로 물 불 전기 자식 만들 예정

---
+++ Source/Prototype/Private/EnergyItem.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnergyItem.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "PrototypeCharacter.h"
+// Sets default values
+AEnergyItem::AEnergyItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("boxComp"));
+    SetRootComponent(boxComp);
+ 
+    boxComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    boxComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+    boxComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+    meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+    meshComp->SetupAttachment(boxComp);
+
+    boxComp->OnComponentBeginOverlap.AddDynamic(this, &AEnergyItem::OnOverlapBegin);
+}
+
+// Called when the game starts or when spawned
+void AEnergyItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnergyItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AEnergyItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+    bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (APrototypeCharacter* Player = Cast<APrototypeCharacter>(OtherActor))
+    {
+        
+
+        // 아이템 제거
+        Destroy();
+    }
+}
---
+++ Source/Prototype/Public/EnergyItem.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnergyItem.generated.h"
+
+UCLASS()
+class PROTOTYPE_API AEnergyItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnergyItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* boxComp;
+
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* meshComp;
+
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult);
+};
