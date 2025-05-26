67ddaa20678f61068b45134ec1a64a07e5a3975b
Yun <86878669+Do1Yun@users.noreply.github.com>
2025-05-26 13:39:11 +0900

    쓸데없는 코드 제거

--- Source/Prototype/Private/ElectricWeapon.cpp
+++ Source/Prototype/Private/ElectricWeapon.cpp
@@ -3,8 +3,6 @@
 #include "PrototypeCharacter.h"
 #include "Camera/CameraComponent.h"
 #include "Kismet/GameplayStatics.h"
-#include "MyAnimInstance.h" // UMyAnimInstance를 사용하기 위해 포함
-
 
 UElectricWeapon::UElectricWeapon()
 {
@@ -28,25 +26,7 @@ void UElectricWeapon::TickComponent(float Dt, ELevelTick, FActorComponentTickFun
 void UElectricWeapon::Fire()
 {
     if (!BulletClass || !OwnerChar) return;
-    // **새로 추가/수정할 부분: 애니메이션 재생 로직**
-    UAnimInstance* AnimInstance = OwnerChar->GetMesh()->GetAnimInstance();
-    if (AnimInstance)
-    {
-        UMyAnimInstance* MyAnim = Cast<UMyAnimInstance>(AnimInstance);
-        if (MyAnim)
-        {
-            MyAnim->PlayfireMontage(); // 캐릭터의 애니메이션 몽타주 재생
-            UE_LOG(LogTemp, Log, TEXT("UElectricWeapon::Fire() -> MyAnim->PlayfireMontage() called!")); // 로그 추가
-        }
-        else
-        {
-            UE_LOG(LogTemp, Warning, TEXT("UElectricWeapon: AnimInstance is not of type UMyAnimInstance!"));
-        }
-    }
-    else
-    {
-        UE_LOG(LogTemp, Warning, TEXT("UElectricWeapon: No AnimInstance found on character mesh!"));
-    }
+
     FVector Muzzle = OwnerChar->GetActorLocation() + OwnerChar->GetActorForwardVector() * 100.f;
     FRotator Dir = OwnerChar->GetActorRotation();
 
