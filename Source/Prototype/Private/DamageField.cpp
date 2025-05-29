682451e0a40b20ce2b97db799ee07a9c4a0ec940
Yun <86878669+Do1Yun@users.noreply.github.com>
2025-05-29 09:29:13 +0900

    DamageField구현 완
    컴포넌트 BP에 붙이고 이벤트그래프에서 overlap시 함수 호출되게 하면됨

--- Source/Prototype/Private/DamageField.cpp
+++ Source/Prototype/Private/DamageField.cpp
@@ -1,16 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "DamageField.h"
+#include "DamageField.h" // 헤더 파일 이름 수정
+#include "PrototypeCharacter.h" // 데미지를 줄 PrototypeCharacter 클래스를 알기 위해 포함
 
 // Sets default values for this component's properties
-UDamageField::UDamageField()
+UDamageField::UDamageField() // 생성자 이름 수정
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 
@@ -18,17 +12,25 @@ UDamageField::UDamageField()
 void UDamageField::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
 }
 
-
-// Called every frame
-void UDamageField::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+// 데미지를 적용하는 함수 구현
+void UDamageField::ApplyDamageToActor(AActor* TargetActor)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	// TargetActor가 유효한지, 그리고 이 컴포넌트를 가진 액터 자신이 아닌지 확인
+	if (!TargetActor || TargetActor == GetOwner())
+	{
+		return;
+	}
 
-	// ...
+	// TargetActor를 APrototypeCharacter로 캐스팅 시도
+	APrototypeCharacter* Character = Cast<APrototypeCharacter>(TargetActor);
+
+	// 캐스팅 성공 시 (즉, 오버랩된 액터가 우리 플레이어 캐릭터일 경우)
+	if (Character)
+	{
+		// 캐릭터에게 데미지 적용
+		UE_LOG(LogTemp, Log, TEXT("DamageField: Applying %d damage to %s"), DamageToDeal, *Character->GetName());
+		Character->TakeDamage(DamageToDeal);
+	}
 }
-
--- Source/Prototype/Public/DamageField.h
+++ Source/Prototype/Public/DamageField.h
@@ -1,28 +1,28 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "DamageField.generated.h"
+#include "DamageField.generated.h" // 파일 이름에 맞춰 수정
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class PROTOTYPE_API UDamageField : public UActorComponent
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class PROTOTYPE_API UDamageField : public UActorComponent // 클래스 이름 수정
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
-	UDamageField();
+	UDamageField(); // 생성자 이름 수정
+
+	// 이 필드가 가하는 데미지량 (에디터 및 블루프린트에서 설정 가능)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Settings")
+	int32 DamageToDeal = 1;
+
+	// 데미지를 적용할 대상 액터를 입력받는 함수 (블루프린트에서 호출 가능)
+	UFUNCTION(BlueprintCallable, Category = "Damage")
+	void ApplyDamageToActor(AActor* TargetActor);
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
 };
