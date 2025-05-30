4920e54a83907340ecc8889176f3f22cc8cb2eaf
Yun <86878669+Do1Yun@users.noreply.github.com>
2025-05-30 15:46:38 +0900

    아 모르겠다 UI
    하트가 왜 안들어가는데~

--- Source/Prototype/Private/PlayerUIWidget.cpp
+++ Source/Prototype/Private/PlayerUIWidget.cpp
@@ -1,5 +1,26 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+#include "PlayerUIWidget.h" // 생성한 파일 이름에 맞게 변경
+#include "PrototypeCharacter.h"   // APrototypeCharacter 참조
 
+void UPlayerUIWidget::InitializeForPlayer(APrototypeCharacter* InPlayerCharacter)
+{
+    PlayerCharacterRef = InPlayerCharacter;
 
-#include "PlayerUIWidget.h"
+    if (PlayerCharacterRef)
+    {
+        // 델리게이트 바인딩
+        PlayerCharacterRef->OnHPChanged.AddDynamic(this, &UPlayerUIWidget::HandleHealthChanged);
 
+        // 초기 HP로 UI 업데이트 호출
+        HandleHealthChanged(PlayerCharacterRef->GetCurrentHP(), PlayerCharacterRef->GetMaxHP());
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerUIWidgetBase: InitializeForPlayer received a null PlayerCharacter."));
+    }
+}
+
+void UPlayerUIWidget::HandleHealthChanged(int32 CurrentHP, int32 MaxHP)
+{
+    // 블루프린트에서 구현된 OnHealthUpdated 이벤트를 호출
+    OnHealthUpdated(CurrentHP, MaxHP);
+}
--- Source/Prototype/Public/PlayerUIWidget.h
+++ Source/Prototype/Public/PlayerUIWidget.h
@@ -1,17 +1,31 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
-#include "PlayerUIWidget.generated.h"
+#include "PlayerUIWidget.generated.h" // 생성한 파일 이름에 맞게 변경
 
-/**
- * 
- */
+class APrototypeCharacter; // 전방 선언
+
 UCLASS()
 class PROTOTYPE_API UPlayerUIWidget : public UUserWidget
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+protected:
+    // 플레이어 캐릭터에 대한 참조
+    UPROPERTY(BlueprintReadOnly, Category = "Setup")
+    APrototypeCharacter* PlayerCharacterRef;
+
+    // 블루프린트에서 UI 업데이트 로직을 구현할 이벤트
+    UFUNCTION(BlueprintImplementableEvent, Category = "UI Update")
+    void OnHealthUpdated(int32 CurrentHP, int32 MaxHP);
+
+    // 캐릭터의 OnHPChanged 델리게이트에 바인딩될 함수
+    UFUNCTION()
+    void HandleHealthChanged(int32 CurrentHP, int32 MaxHP);
+
+public:
+    // 블루프린트에서 호출하여 캐릭터 참조를 설정하고 델리게이트를 바인딩하는 함수
+    UFUNCTION(BlueprintCallable, Category = "Setup")
+    void InitializeForPlayer(APrototypeCharacter* InPlayerCharacter);
 };
